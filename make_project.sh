d1e67f09de8eac271db8c99b36971cba894daca3
Mariia Gladkova <gladkovamaria14@gmail.com>
2021-03-09 13:48:37 +0100

    Add PyTorch dependency and installation commands

--- make_project.sh
+++ make_project.sh
@@ -17,9 +17,13 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
 make -j$NUM_PROC
 
+cd "$BASEDIR/thirdparty/"
+wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-cxx11-abi-shared-with-deps-latest.zip
+unzip libtorch-cxx11-abi-shared-with-deps-latest.zip
+rm libtorch-cxx11-abi-shared-with-deps-latest.zip
+
 cd "$BASEDIR"
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_PREFIX_PATH=$BASEDIR/thirdparty/libtorch ..
 make -j$NUM_PROC
-
